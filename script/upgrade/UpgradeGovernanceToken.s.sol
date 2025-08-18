4e38a20acc0eb20c824a0a3d46f6ca89616c349c
jnmclarty <jeffrey.mclarty@gmail.com>
2025-08-18 13:23:33 -0400

    Working?

--- script/upgrade/UpgradeGovernanceToken.s.sol
+++ script/upgrade/UpgradeGovernanceToken.s.sol
@@ -13,19 +13,21 @@ contract UpgradeGovernanceToken is Script {
     function run() external {
         // Proxy address (the one users interact with)
         address proxyAddress = 0xE3D4a55f780C5aD9B3009523CB6a3d900A8FA723;
-        // address proxyAddress = 0x309E4466149d2E6286978680f4f9e2726A999E43;
+        //address proxyAddress = 0x309E4466149d2E6286978680f4f9e2726A999E43;
 
         vm.startBroadcast(); // must be the admin EOA here
 
         // Deploy new implementation
         GovernanceToken newImplementation = new GovernanceToken();
 
+        GovernanceToken(proxyAddress).upgradeToAndCall(address(newImplementation), bytes(""));
+
         // Call upgradeTo directly on the proxy
-        GovernanceToken(proxyAddress).upgradeToAndCall(address(newImplementation), abi.encodeWithSelector(GovernanceToken.initialize.selector,
-                        0xA622279f76ddbed4f2CC986c09244262Dba8f4Ba,
-                        0xF5B87BD1206d7658C344Fe1CB56D9498B4286A67,
-                        "ProtocolGuild",
-                        "PGUILD"));
+        // GovernanceToken(proxyAddress).upgradeToAndCall(address(newImplementation), abi.encodeWithSelector(GovernanceToken.initialize.selector,
+        //                0xA622279f76ddbed4f2CC986c09244262Dba8f4Ba,
+        //                0xF5B87BD1206d7658C344Fe1CB56D9498B4286A67,
+        //                "ProtocolGuild",
+        //                "PGUILD"));
 
         vm.stopBroadcast();
     }
